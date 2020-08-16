0560436aab51d4d0e33cbc80bcb42acc44783a08
Pranay-kumar-123 <vorugantipranay@gmail.com>
2020-08-16 17:15:27 +0530

    CHANGED

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -42,7 +42,7 @@ $(function(){
 
 
 function display(data){
-    var txt="<table><tr><th>ID</th><th>NAME</th><th>AGE</th><th>MOBILE</th></tr>";
+    var txt="<table><tr><th>ID</th><th>FIRST NAME</th><th>LAST NAME</th><th>AGE</th><th>MOBILE</th></tr>";
      for(var i=0;i<data.length;i++){
          txt+="<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].lname+"</td><td>"+data[i].age+"</td><td>"+data[i].mobile+"</td"+"</tr>";
      }
