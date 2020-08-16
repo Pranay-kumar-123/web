f42b1e72df82a8ee5bd884dcf31b6b408b85d6a6
Pranay-kumar-123 <vorugantipranay@gmail.com>
2020-08-16 17:12:35 +0530

    changed

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -44,7 +44,7 @@ $(function(){
 function display(data){
     var txt="<table><tr><th>ID</th><th>NAME</th><th>AGE</th><th>MOBILE</th></tr>";
      for(var i=0;i<data.length;i++){
-         txt+="<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].age+"</td><td>"+data[i].mobile+"</td"+"</tr>";
+         txt+="<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].lname+"</td><td>"+data[i].age+"</td><td>"+data[i].mobile+"</td"+"</tr>";
      }
      txt+="</table>";
      $("#table").html(txt);
